c14a2aeb3c59430ee949a7721fa7bbbfeaf54515
Ross Condie <rosscondie@gmail.com>
2023-01-25 20:10:25 +0100

    functions_lab

---
+++ src/python_functions_practice.py
@@ -0,0 +1,27 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2 
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(month_num):
+    return month_num + "January"
+    
