eb2d41229e87a936a432a7674bd85a88970ea9e9
Ross Condie <rosscondie@gmail.com>
2023-01-26 11:06:20 +0100

    update after homework review

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -22,30 +22,44 @@ def join_string(string_1, string_2):
 def add_string_as_number(string_1, string_2):
     return int(string_1) + int(string_2)
 
+# def number_to_full_month_name(month):
+#     if month == 1:
+#         return "January"
+#     elif month == 3:
+#         return "March"
+#     elif month == 9:
+#         return "September"
+
 def number_to_full_month_name(month):
     if month == 1:
         return "January"
     elif month == 3:
         return "March"
+    elif month == 4:
+        return "April"
     elif month == 9:
         return "September"
+    elif month == 10:
+        return "October"
+
+def number_to_short_month_name(month):
+    return number_to_full_month_name(month)[0:3]
+
+# def number_to_short_month_name(month_name):
+#     if month_name == 1:
+#         return "Jan"
+#     elif month_name == 4:
+#         return "Apr"
+#     elif month_name == 10:
+#         return "Oct"
 
 
-def number_to_short_month_name(month_name):
-    if month_name == 1:
-        return "Jan"
-    elif month_name == 4:
-        return "Apr"
-    elif month_name == 10:
-        return "Oct"
+# def cube():
+#     return 2 * 2 * 2
 
-
-#I know these last two functions pass but I am sure there is a better way to write them!
-
-def cube():
-    return 2 * 2 * 2
-
-    
+def volume_of_cube(length_of_side):
+    return length_of_side **3
+        
 def reverse_string():
     return reversed()
 
